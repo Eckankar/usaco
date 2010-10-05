14000423e5d50ac0f40e8a6f69ee19c4b90c3264
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-10-05 23:29:56 +0200

    palsquare done.

--- section1.2/palsquare.cpp
+++ section1.2/palsquare.cpp
@@ -7,10 +7,39 @@ LANG: C++
 
 using namespace std;
 
+void itoa(int n, int base, string &out) {
+    out = "";
+    for (; n > 0; n /= base) {
+        out = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[n % base] + out;
+    }
+}
+
+bool is_palindrome(string s) {
+    int len = s.length();
+    for (int i = 0; i < len/2; i++) {
+        if (s[i] != s[len-i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    ifstream fin("namenum.in");
-    ofstream fout("namenum.out");
+    ifstream fin("palsquare.in");
+    ofstream fout("palsquare.out");
 
+    int b;
+    fin >> b;
 
+    string num;
+    for (int i = 1; i <= 300; i++) {
+        itoa(i*i, b, num);
+        if (is_palindrome(num)) {
+            string n;
+            itoa(i, b, n);
+            fout << n << " " << num << endl;
+
+        }
+    }
 }
 
